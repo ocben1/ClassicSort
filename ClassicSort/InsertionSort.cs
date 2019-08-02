44328902d1dd82c0a2c3573789a6353b56040464
Oliver <ocben1@gmail.com>
2019-08-03 03:52:40 +1000

    added comments for insertion sort

--- ClassicSort/InsertionSort.cs
+++ ClassicSort/InsertionSort.cs
@@ -4,21 +4,28 @@ using System.Text;
 
 namespace Vector
 {
+    //to do REFACTOR.
+    //code adapted from https://www.codesdope.com/blog/article/sorting-an-array-using-insertion-sort-in-c/
     public class InsertionSort : ISorter
     {
         public void Sort<K>(K[] sequence, IComparer<K> comparer) where K : IComparable<K>
         {
-            for (int i = 0; i < sequence.Length; i++)
+            for (int i = 1; i < sequence.Length; i++)
             {
-                int j = i;
-                while ((j > 0) && comparer.Compare(sequence[j - 1], sequence[j]) > 0)
+                int j = i; //selects the first unsorted element
+                //this while loop shifts all the elements to the right to create the position for the
+                //unsorted element
+                while ((j > 0) && comparer.Compare(sequence[j - 1], sequence[j]) > 0) //while the elements arent sorted..
                 {
+                    //perform the insertion as a variable swap
                     K tmp = sequence[j];
                     sequence[j] = sequence[j - 1];
                     sequence[j - 1] = tmp;
                     sequence[j] = sequence[j - 1];
+                    //then consider previous element for comparison
                     j = j - 1;
                 }
+                
             }
         }
     }
